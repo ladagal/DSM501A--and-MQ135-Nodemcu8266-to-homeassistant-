fba9fd317ebff32e4d4cc18a3935f319f1ee4cf3
ladagal <53316434+ladagal@users.noreply.github.com>
2024-01-05 21:06:42 +0100

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -1,5 +1,5 @@
-#define SECRET_SSID ""		// Wifi SSID
-#define SECRET_PASS ""	// Heslo k Wifi
+#define SECRET_SSID ""	     	// Wifi SSID
+#define SECRET_PASS ""	      // Heslo k Wifi
 #define MQTT_USERNAME ""			// Jmeno do MQTT
-#define MQTT_PASSWORD ""   //Heslo do MQTT
-#define MQTT_BROKER ""  // Adresa MQTT brokeru
+#define MQTT_PASSWORD ""      // Heslo do MQTT
+#define MQTT_BROKER ""        // Adresa MQTT brokeru
